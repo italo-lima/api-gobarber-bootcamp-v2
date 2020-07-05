e4f40dbd2d0baa8afe11b5b1e7a33b1981529f51
italo-lima <italojonas@hotmail.com>
2020-07-05 19:40:59 -0300

    finished forgot and reset password

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,9 +3,18 @@ import { container } from 'tsyringe';
 import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
 
-// import IMailProvider from "@shared/container/providers/MailProvider/models/IMailProvider"
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import EtherealMailProvider from '@shared/container/providers/MailProvider/implementations/EtherealMailProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
+);
+
+/* Ethereal não funcionou com singleton, por isso foi
+   criado com registerInstance, similar ao singleton
+*/
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMailProvider(),
 );
