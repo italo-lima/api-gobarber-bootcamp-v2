43d0d207d6652868998ddc7ef58bec7d9616f893
italo-lima <italojonas@hotmail.com>
2020-07-07 13:30:43 -0300

    finished provider  month and day availability

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,24 @@ import { Router } from 'express';
 
 import verifyAuthentication from '@modules/users/infra/http/middlewares/verifyAuthentication';
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
+import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController';
 
 const providersRoutes = Router();
 const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 providersRoutes.use(verifyAuthentication);
 
 providersRoutes.get('/', providersController.index);
+providersRoutes.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
+providersRoutes.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index,
+);
 
 export default providersRoutes;
--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -14,10 +14,14 @@ describe('CreateAppointment', () => {
   });
 
   it('should be able to create a new appointment', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
     const appointment = await createAppointmentService.execute({
       provider_id: '123',
       user_id: '1234',
-      date: new Date(),
+      date: new Date(2020, 4, 10, 13),
     });
 
     expect(appointment).toHaveProperty('id');
@@ -25,7 +29,7 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointments on the same time', async () => {
-    const appointmentDate = new Date(2020, 4, 1, 11);
+    const appointmentDate = new Date(2020, 11, 10, 11);
 
     await createAppointmentService.execute({
       provider_id: '123',
@@ -38,6 +42,56 @@ describe('CreateAppointment', () => {
         provider_id: '123',
         user_id: '1234',
         date: appointmentDate,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create appointment on a past date', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointmentService.execute({
+        date: new Date(2020, 4, 10, 10),
+        user_id: '123',
+        provider_id: '321',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create appointment an appointment with same user as provider', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointmentService.execute({
+        date: new Date(2020, 4, 10, 13),
+        user_id: '123',
+        provider_id: '123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create appointment an appointment before 8am and after 5pm', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointmentService.execute({
+        date: new Date(2020, 4, 11, 7),
+        user_id: '123',
+        provider_id: '1234',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+
+    await expect(
+      createAppointmentService.execute({
+        date: new Date(2020, 4, 11, 18),
+        user_id: '123',
+        provider_id: '1234',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
